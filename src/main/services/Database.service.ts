bc749e25d14453112910b71b680f120f68d4c5b4
smrifat1411 <smrifat1411@gmail.com>
2023-12-07 22:10:49 +0600

    test for windows

--- src/main/services/Database.service.ts
+++ src/main/services/Database.service.ts
@@ -1,5 +1,6 @@
 import Database from 'better-sqlite3';
 import path from 'path';
+import fs from 'fs';
 
 export type TODO = {
   id?: number;
@@ -11,15 +12,30 @@ export type TODO = {
 const isDevelopment =
   process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
 
+let cachedConnection: any | null = null;
+
 export function connect() {
+  if (cachedConnection) {
+    return cachedConnection;
+  }
+
   const databasePath = isDevelopment
     ? path.join(__dirname, '../../../', 'release/app', 'database.db')
     : path
         .join(__dirname, '../../database.db')
         .replace('app.asar', 'app.asar.unpacked');
 
-  return Database(path.resolve(databasePath), {
+  // Check if the database file exists
+  const doesDatabaseExist = fs.existsSync(databasePath);
+
+  if (!doesDatabaseExist) {
+    console.log('Creating a new database connection');
+  }
+
+  cachedConnection = Database(path.resolve(databasePath), {
     verbose: console.log,
     fileMustExist: true,
   });
+
+  return cachedConnection;
 }
